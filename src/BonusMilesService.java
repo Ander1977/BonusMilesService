9ecba6dbda4e13ded19e4741b434fd85a57d5e69
Andrei Kopatilov <koopa-andrej@live.com>
2020-05-19 16:06:46 +0300

    completion

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,5 @@
 public class BonusMilesService {
-    public int calculate (int cost) {
-        int price = 10_000;
-        int bonus = 20;
+    public int calculate (int price, int bonus) {
         int miles = price / bonus;
         return miles;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int miles = service.calculate(price);
+        int price;
+        int bonus;
+        int miles = service.calculate(price = 60_000, bonus = 20);
         System.out.println(miles);
     }
 }
